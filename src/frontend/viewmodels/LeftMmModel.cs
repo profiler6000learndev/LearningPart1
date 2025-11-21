11982e380f80c8c82dad176562e7a207c6bb68e3
Enrico Meister <profiler5000@gmail.com>
2025-11-21 16:02:19 +0100

    fertig angelegt und struktur mit basemodel implementiert

---
+++ src/frontend/viewmodels/LeftMmModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frontend.viewmodels
+{
+    public class LeftMmModel : BaseModel
+    {
+        public ObservableCollection<string> ButtonTextItem { get; set; }
+
+        public LeftMmModel()
+        {
+            ButtonTextItem = new ObservableCollection<string>
+            {
+              "Neues Spiel",
+              "Spiel Laden",
+                "Einstellungen",
+                "Anleitung",
+                "Patchnotes",
+            };
+        }
+
+    }
+}
