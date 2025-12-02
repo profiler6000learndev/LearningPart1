06d5dbbc355859d4e7bc5b3f7fd04121ee5b1278
Enrico Meister <profiler5000@gmail.com>
2025-12-02 15:57:36 +0100

    #12 Game Window erstellt - Header für GameWindow erstellt

---
+++ src/frontend/viewmodels/LeftGameMmModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace frontend.viewmodels
+{
+    class LeftGameMmModel : BaseModel
+            {
+        public MenuItem _item { get; }
+        public ObservableCollection<MenuItem> Items = new ObservableCollection<MenuItem>();
+            
+    
+        public LeftGameMmModel()
+        {
+             _item = new MenuItem() { Header = null };
+            Items.Add(_item);
+        }
+        
+
+    }
+}
